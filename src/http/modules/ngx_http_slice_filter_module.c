b8930636ea02ad7ec182ea65f0b1c79e0e2d2f46
amirhossein.vz <hosseinvadi7777@gmail.com>
2019-02-27 22:07:15 +0330

    change subrequest flag to not inherit phase and handler

--- src/http/modules/ngx_http_slice_filter_module.c
+++ src/http/modules/ngx_http_slice_filter_module.c
@@ -280,8 +280,7 @@ ngx_http_slice_body_filter(ngx_http_request_t *r, ngx_chain_t *in)
         return rc;
     }
 
-    if (ngx_http_subrequest(r, &r->uri, &r->args, &ctx->sr, NULL,
-                            NGX_HTTP_SUBREQUEST_CLONE)
+    if (ngx_http_subrequest(r, &r->uri, &r->args, &ctx->sr, NULL, 0)
         != NGX_OK)
     {
         return NGX_ERROR;
