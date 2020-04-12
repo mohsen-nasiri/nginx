6345a93e44488b4117c5aa9b44bc1b4275f79e7b
a.hossein <amirhossein@arvancloud.com>
2020-04-12 17:43:37 -0400

    scenario 1

--- src/http/ngx_http_upstream.h
+++ src/http/ngx_http_upstream.h
@@ -170,6 +170,7 @@ typedef struct {
 
     ngx_http_complex_value_t         *ignore_headers;
     ngx_uint_t                       next_upstream;
+    ngx_http_complex_value_t         *next_upstream_tcp;
     ngx_uint_t                       store_access;
     ngx_uint_t                       next_upstream_tries;
     ngx_flag_t                       buffering;
